9048f3183369ce3169fc3d72a33485071fae120f
eyvind <eyvind@192.168.0.107>
2019-06-15 14:06:49 +0200

    sdfsf

--- src/filetodb/FileToDB.java
+++ src/filetodb/FileToDB.java
@@ -1,6 +1,7 @@
 package filetodb;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
@@ -11,74 +12,69 @@ import javax.persistence.Query;
 
 public class FileToDB {
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
         FileToDB t = new FileToDB();
-        t.readFromFile();
         t.persist();
     }
-    
-    private void readFromFile()
-    {
-        String fileName = "src/filetodb/test.csv";
-        String textToRead = null;
-        int counter=0;
 
-		try (BufferedReader instream = new BufferedReader(new FileReader(
-				fileName))) {
-			while ((textToRead = instream.readLine()) != null) {
-                            
-                            if(counter>0)
-			System.out.println(textToRead);
-                            counter++;
-				
-			}
-
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e1) {
-			e1.printStackTrace();
-		}
-        
-    }
 
     private void persist() {
+        File file=new File("src/filetodb/test.csv");
+        String textToRead = null;
+        int counter = 0;
         EntityManagerFactory emf = javax.persistence.Persistence.createEntityManagerFactory("FileToDBPU");
         EntityManager em = emf.createEntityManager();
         em.getTransaction().begin();
         Query q = em.createQuery("select m from Money m");
-        int size=q.getResultList().size();
-        if(size>0)
+        int size = q.getResultList().size();
+        if (size > 0) {
             return;
-try {
-        StringTokenizer tokenizer = new StringTokenizer("12;30.23;SEK", ";");//test
-        long id = 0;
-        double amount = 0;
-        while (tokenizer.hasMoreTokens()) {
-            String s1 = tokenizer.nextToken();
-            String s2 = tokenizer.nextToken();
-            String s3 = tokenizer.nextToken();
-            try {
-                id = Long.parseLong(s1);
-                amount = Double.parseDouble(s2);
+        }
+        try {
 
-            } catch (NumberFormatException e) {
+            try (BufferedReader instream = new BufferedReader(new FileReader(
+                    file))) {
+                while ((textToRead = instream.readLine()) != null) {
+
+                    if (counter > 0) {
+
+                        StringTokenizer tokenizer = new StringTokenizer(textToRead,";");
+                        long id = 0;
+                        double amount = 0;
+                        while (tokenizer.hasMoreTokens()) {
+                            String s1 = tokenizer.nextToken();
+                            String s2 = tokenizer.nextToken();
+                            String s3 = tokenizer.nextToken();
+                            try {
+                                id = Long.parseLong(s1);
+                                amount = Double.parseDouble(s2);
+
+                            } catch (NumberFormatException e) {
+                                e.printStackTrace();
+                            }
+
+                            Money money = new Money(id, amount, s3);
+                            em.persist(money);
+
+                        }
+
+                    }
+                    counter++;
+
+                }
+
+            } catch (FileNotFoundException e) {
                 e.printStackTrace();
+            } catch (IOException e1) {
+                e1.printStackTrace();
             }
 
-            Money money = new Money(id, amount, s3);
-            em.persist(money);
-
-        }
-
-        
-            
             em.getTransaction().commit();
         } catch (Exception e) {
             em.getTransaction().rollback();
         } finally {
+            file.delete();
             em.close();
         }
 
